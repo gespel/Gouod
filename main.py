35639e2f58dd214490c5e6147a3a81b364f289e9
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2022-10-26 19:29:00 +0200

    formating

--- main.py
+++ main.py
@@ -8,12 +8,7 @@ p = pyaudio.PyAudio()
 
 d: Module = StrangeOne()
 
-
 sampleslist = []
-#np.array([])
-# generate samples, note conversion to float32 array
-#samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-
 
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
@@ -21,26 +16,13 @@ stream = p.open(format=pyaudio.paFloat32,
                 output=True,
                 frames_per_buffer=1024)
 
-while(True):
+while True:
     samples = np.array([])
     sampleslist = []
-    for i in range(0,1023):
+    for i in range(0, 1023):
         sampleslist.append(d.get_sample())
-        #sampleslist.append(x.get_sample())
-        #x.set_frequency(abs(y.get_sample()) * 440)
-        #y.set_frequency(abs(z.get_sample()) * 2)
-        #z.set_frequency(v.get_sample() * 3)
-        
+
     samples = np.array(sampleslist).astype(np.float32)
-    #samples *= v.getSample()
     samples *= 0.5
-   
-    #samples = x.getBuffer()
-    #samples += y.getBuffer()
-    #samples *= 0.2
+
     stream.write(samples.tobytes())
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
